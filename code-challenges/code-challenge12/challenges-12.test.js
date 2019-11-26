03e2c2a09f057e0b267d9a75c52a93a43c9648b3
ghaid01 <ghaid.zahran2001@gmail.com>
2019-11-26 22:17:21 +0200

    2 challenges completed

--- code-challenges/code-challenge12/challenges-12.test.js
+++ code-challenges/code-challenge12/challenges-12.test.js
@@ -23,8 +23,13 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-  // Solution code here...
-
+  return stores.reduce((acc, store) => {
+    for (let i = 0; i < store.length; i++) {
+      if (!acc[i]) acc[i] = 0;
+      acc[i] += store[i];
+    }
+    return acc;
+  }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -38,7 +43,14 @@ Write a function named salesData that uses forEach to iterate over the hourlySal
 ------------------------------------------------------------------------------------------------ */
 
 const salesData = (hours, data) => {
-  // Solution code here...
+  const sales = [];
+  data.forEach((d, idx) => {
+    sales.push({
+      sales: d + ' cookies',
+      time: hours[idx]
+    });
+  });
+  return sales;
 };
 
 /* ------------------------------------------------------------------------------------------------
